a821e3baefe77bd5bf81b09c0ce195c8716f7712
whats-mick <9634573+mick-whats@users.noreply.github.com>
2018-12-02 18:18:40 +0900

    rm and mv

---
+++ src/index.js
@@ -0,0 +1,10 @@
+class Sample {
+  constructor (name) {
+    this.name = 'name'
+  }
+  hello () {
+    return `Hello ${this.name}`
+  }
+}
+
+module.exports = Sample
--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-const test = require('ava').test;
-const Sample = require('../lib/index');
-
-test('sample hello', (t) => {
-  const mick = new Sample('Mick');
-  t.is(mick.hello(), 'Hello Mick');
-});
